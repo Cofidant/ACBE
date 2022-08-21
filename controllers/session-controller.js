20122315aebe8672b94b6686c77524bdbc3f8c31
MAHADI ABUHURAIRA <mamt4real@gmail.com>
2022-08-21 16:25:03 +0100

    comment console .log

--- controllers/session-controller.js
+++ controllers/session-controller.js
@@ -1,13 +1,10 @@
 const Session = require('../models/Session')
 // const catchAsync = require('../utils/catchAsync')
 const factoryController = require('./handlerFactory')
-const sessionService = require('../services')
+
+// start Session Services
+require('../services')
 
 exports.getAllSessions = factoryController.getAll(Session)
 exports.createTherapySession = factoryController.createOne(Session)
 exports.getOneSession = factoryController.getOne(Session)
-
-Session.find().then(res =>{
-    console.log(res)
-})
-
